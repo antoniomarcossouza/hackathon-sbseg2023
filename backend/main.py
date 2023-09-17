31754139da3cae9ee341ae480283a674740e78e9
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2023-09-17 15:13:16 -0300

    classificação de alertas

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 
 app = Flask(__name__)
@@ -10,12 +10,11 @@ db_config = {
     "port": "5432",
 }
 
-
 def fetch_data(table_name: str) -> list:
     try:
         with psycopg2.connect(**db_config) as connection:
             with connection.cursor() as cursor:
-                query = f"SELECT * FROM {table_name};"
+                query = f"SELECT * FROM {table_name} LIMIT 1000;"
                 cursor.execute(query)
 
                 result = cursor.fetchall()
@@ -27,10 +26,63 @@ def fetch_data(table_name: str) -> list:
     except Exception as e:
         print(f"Error fetching data: {str(e)}")
 
+def run_query(query: str) -> None:
+    with psycopg2.connect(**db_config) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+        conn.commit()
 
-@app.route("/alerts", methods=["GET"])
+def whois(ip: str) -> str:
+    return f'WhoIs: {ip}'
+
+def check_open_port(ip: str, port: int) -> bool:
+    return 0
+
+@app.route("/alerts", methods=["GET", "POST"])
 def show_alerts():
-    return jsonify(fetch_data(table_name="suricata_logs"))
+    match request.method:
+        case "GET":
+            return jsonify(fetch_data(table_name="suricata_logs"))
+        case "POST":
+            json = request.get_json()
+            query = f"""
+INSERT INTO alerts_responses(
+        alert_severity,
+        alert_category,
+        alert_signature,
+        payload,
+        src_ip,
+        whois_src_ip,
+        src_port,
+        dest_ip,
+        whois_dest_ip,
+        dest_port,
+        dest_port_open,
+        incitent,
+        timestamp
+    )
+VALUES (
+        { json ['alert_severity'] },
+        '{json['alert_category']}',
+        '{json['alert_signature']}',
+        '{json['payload']}',
+        '{json['src_ip']}',
+        '{whois(json['src_ip'])}',
+        { json ['src_port'] },
+        '{json['dest_ip']}',
+        '{whois(json['dest_ip'])}',
+        { json ['dest_port'] },
+        '{check_open_port(json['dest_port'], json['dest_port'])}',
+        { json ['is_incident'] },
+        '{json['timestamp']}'
+    );"""
+            try:
+                run_query(query=query)
+                if json['is_incident']:
+                    print('Envia request para Request Tracker')
+                return "Success", 200
+            except Exception as e:
+                return "Internal Server Error", 500
 
 
 if __name__ == "__main__":
